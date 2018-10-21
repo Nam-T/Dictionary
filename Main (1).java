79dbce23c9a8c8bef6957ec046c7f5b1b1e4f122
Nam-T <37497417+Nam-T@users.noreply.github.com>
2018-10-21 23:12:33 +0700

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,14 @@
+package diction;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		new DicGUI();
+
+	}
+
+}
